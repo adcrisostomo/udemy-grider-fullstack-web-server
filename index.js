d892935ac097b80a74c10acf13ad87e58c2d136f
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-11-24 00:30:58 +0800

    Setup routes, services, and add ES linter

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const app = express() // listens for requests
-
-app.get('/', (req, res) => {
-    res.send({bye: 'buddy'})
-})
+const app = express()
+require('./services/passport')
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT) // localhost:5000 (if dev build)
+app.listen(PORT) // if dev build, then localhost:5000
