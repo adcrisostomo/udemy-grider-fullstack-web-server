f2957cf718afbc97e50fa7621b6b5303fdd3330d
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-11-24 01:53:00 +0800

    Connect mongoose to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 const app = express()
 require('./services/passport')
 require('./routes/authRoutes')(app)
+
+mongoose.connect(keys.mongoURI)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT) // if dev build, then localhost:5000
