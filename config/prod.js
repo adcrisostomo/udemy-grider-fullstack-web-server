edff1770cc47cadce54532a0d6dbec0c5b16587e
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-11-24 17:42:32 +0800

    Setup dev and prod keys

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// FOR PRODUCTION VERSION
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+}
