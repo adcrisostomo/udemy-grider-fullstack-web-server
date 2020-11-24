dd65a664ec80c7a81243d0ea8a739db7b0885945
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-11-25 03:54:41 +0800

    Setup materializeCSS, redux actions, and redux thunk

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux' // this is the super parent of all Components
 import { createStore, applyMiddleware } from 'redux'
+// if webpack detects pathing is not relative...
+// ...it assumes you are importing from node_modules directory
+// this will also automatically affect all Component's CSS
+import 'materialize-css/dist/css/materialize.min.css'
+import reduxThunk from 'redux-thunk'
 import App from './components/App'
 import reducers from './reducers'
 
-const store = createStore(() => [], {}, applyMiddleware())
+const store = createStore(() => [], {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(
     <Provider store={store}>
