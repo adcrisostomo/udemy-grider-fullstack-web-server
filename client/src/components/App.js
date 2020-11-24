1b74361cc3aa4e630b600d4748aa2baebae9997c
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-11-25 01:32:00 +0800

    Setup store and Provider

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => { // this is functional component
+    return(
+        <div>
+            Hi There!
+        </div>
+    )
+}
+
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Root of all dev-written React code
+// Redux side of code
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux' // this is the super parent of all Components
+import { createStore, applyMiddleware } from 'redux'
+import App from './components/App'
+
+const store = createStore(() => [], {}, applyMiddleware())
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Provider store={store}>
+        <App/>
+    </Provider>, // react component in jsx format
+    document.querySelector('#root') // html id
+)
