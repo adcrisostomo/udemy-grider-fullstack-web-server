4ed38738ae5be4c0937caabedcd3a0dbd1eb9915
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-11-27 01:42:34 +0800

    Setup Survey and Recipient schemas

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const RecipientSchema = require('./Recipient') // require sub-document collection
 
 const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
-    recipients: [ String ]
+    recipients: [ RecipientSchema ],
+    yes: {
+        type: Number,
+        default: 0
+    },
+    no: {
+        type: Number,
+        default: 0
+    },
+    _user: { // this is reference/relationship field
+        type: Schema.Types.ObjectId, // foreign key equivalent from SQL-based DB's
+        ref: 'User'
+    },
+    dateSent: Date,
+    lastResponded: Date
 })
 
 mongoose.model('surveys', surveySchema) // create this table in the db
