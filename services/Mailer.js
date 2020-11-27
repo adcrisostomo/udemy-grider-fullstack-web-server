42b7d6439528d3991adf2db61378cea82a745063
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-11-27 16:01:18 +0800

    Remove unnecessary await

--- services/Mailer.js
+++ services/Mailer.js
@@ -44,7 +44,7 @@ class Mailer extends helper.Mail {
 
     async send () {
         // prep request
-        const request = await this.sgApi.emptyRequest({
+        const request = this.sgApi.emptyRequest({
             method: 'POST',
             path: '/v3/mail/send',
             body: this.toJSON()
