2d56abff72d7e4e913a84b61ee1dad5302fb5b8e
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-12-01 20:32:08 +0800

    Add SurveyFormReview to SurveyNew

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,27 @@
-// parent of SurveyForm and SurveyReview
+// parent of SurveyForm and SurveyFormReview
 import React, { Component } from 'react'
 import SurveyForm from './SurveyForm'
+import SurveyFormReview from './SurveyFormReview'
 
 class SurveyNew extends Component {
+    state = { showFormReview: false } // newer syntax sugar
+
+    renderContent () {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview
+                onCancel={() => this.setState({ showFormReview: false })}
+            />
+        } else {
+            return <SurveyForm
+                onSurveySubmit={() => this.setState({ showFormReview: true })}
+            />
+        }
+    }
+
     render() {
         return (
             <div>
-                <SurveyForm />
+                { this.renderContent() }
             </div>
         )
     }
