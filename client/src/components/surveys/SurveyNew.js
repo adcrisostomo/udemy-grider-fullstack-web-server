bba9997a20d93082facd2c36a9867b923840201c
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-12-01 19:10:30 +0800

    Add SurveyNew component

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,12 @@
+// parent of SurveyForm and SurveyReview
 import React, { Component } from 'react'
+import SurveyForm from './SurveyForm'
 
 class SurveyNew extends Component {
     render() {
         return (
             <div>
-                SurveyNew!
+                <SurveyForm />
             </div>
         )
     }
