f8552db4b1442d2cc4d843056e453efe18cb4496
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-12-01 20:32:30 +0800

    Contain form fields in a separate, importable file

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: 'Survey Title', name: 'title', errorMessage: 'Please provide a survey title' },
+    { label: 'Subject Line', name: 'subject', errorMessage: 'Please provide a subject' },
+    { label: 'Email Body', name: 'body', errorMessage: 'Please provide an email body' },
+    { label: 'Recipient List', name: 'emails', errorMessage: 'Please provide at least one (1) email address' },
+]
