c6fca34a9378041c3bb94c271b263758fc5366d8
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-12-02 22:38:17 +0800

    Add withRouter to SurveyFormReview

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,10 @@ import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux'
 import formFields from './formFields'
+import { withRouter } from 'react-router-dom'
+import * as actions from '../../actions'
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = _.map(formFields, ({ label, name }) => {
         return (
             <div key={name}>
@@ -27,6 +29,7 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
                 Back
             </button>
             <button
+                onClick={() => submitSurvey(formValues, history)}
                 className='green btn-flat right white-text'
             >
                 Send Survey <i className='material-icons right'>email</i>
@@ -39,4 +42,5 @@ function mapStateToProps (state) {
     return { formValues: state.form.surveyForm.values }
 }
 
-export default connect(mapStateToProps)(SurveyFormReview)
+// connect mapStateToProps' and actions' contents to this component's state 
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview))
