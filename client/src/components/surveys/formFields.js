c0da9462098003c1db425f317f7ff31f0d70ca11
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-12-02 22:36:09 +0800

    Change identifier for emails

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
     { label: 'Survey Title', name: 'title', errorMessage: 'Please provide a survey title' },
     { label: 'Subject Line', name: 'subject', errorMessage: 'Please provide a subject' },
     { label: 'Email Body', name: 'body', errorMessage: 'Please provide an email body' },
-    { label: 'Recipient List', name: 'emails', errorMessage: 'Please provide at least one (1) email address' },
+    { label: 'Recipient List', name: 'recipients', errorMessage: 'Please provide at least one (1) email address' },
 ]
