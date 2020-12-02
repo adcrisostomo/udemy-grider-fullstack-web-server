92df5212b368208e7285f46f548c7d7ad2d6f331
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-12-02 22:35:36 +0800

    Improve action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,8 +12,20 @@ export const fetchUser = () => async dispatch => {
     })
 }
 
-export const handleToken = (token) => async dispatch => {
+export const handleToken = token => async dispatch => {
     const res = await axios.post('/api/stripe', token)
+
+    dispatch({
+        type: FETCH_USER,
+        payload: res.data
+    })
+}
+
+export const submitSurvey = (values, history) => async dispatch => {
+    const res = await axios.post('/api/surveys', values)
+
+    // redirect to this path
+    history.push('/surveys')
 
     dispatch({
         type: FETCH_USER,
