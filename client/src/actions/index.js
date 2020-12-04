e5c094434e9f9a0ae506cbeed4f4efbc727eee1d
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2020-12-04 16:36:17 +0800

    Add fetchSurveys action and FETCH_SURVEYS action type

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { FETCH_USER } from './types'
+import { FETCH_USER, FETCH_SURVEYS } from './types'
 
 // redux-thunk sees that this returns a function...
 export const fetchUser = () => async dispatch => {
@@ -29,6 +29,15 @@ export const submitSurvey = (values, history) => async dispatch => {
 
     dispatch({
         type: FETCH_USER,
+        payload: res.data
+    })
+}
+
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys')
+
+    dispatch({
+        type: FETCH_SURVEYS,
         payload: res.data
     })
 }
